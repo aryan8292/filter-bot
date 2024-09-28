2269ad0b13c4336a069ea5a115cc7416320d104b
Rajps55 <hackinggm69@gmail.com>
2024-09-28 12:52:24 +0530

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,109 @@
+import asyncio
+from pyrogram import Client
+from database.ia_filterdb import Media
+from aiohttp import web
+from database.users_chats_db import db
+from web import web_app
+from info import LOG_CHANNEL, API_ID, API_HASH, BOT_TOKEN, PORT, BIN_CHANNEL, ADMINS, DATABASE_URL
+from utils import temp, get_readable_time, save_group_settings
+from typing import Union, Optional, AsyncGenerator
+from pyrogram import types
+import time, os, platform
+from pyrogram.errors import AccessTokenExpired, AccessTokenInvalid, FloodWait
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+
+class Bot(Client):
+    def __init__(self):
+        super().__init__(
+            name='Auto_Filter_Bot',
+            api_id=API_ID,
+            api_hash=API_HASH,
+            bot_token=BOT_TOKEN,
+            plugins={"root": "plugins"}
+        )
+
+    async def start(self):
+        temp.START_TIME = time.time()
+        b_users, b_chats = await db.get_banned()
+        temp.BANNED_USERS = b_users
+        temp.BANNED_CHATS = b_chats
+        
+        # MongoDB connection
+        client = MongoClient(DATABASE_URL, server_api=ServerApi('1'))
+        try:
+            client.admin.command('ping')
+            print("Pinged your deployment. You successfully connected to MongoDB!")
+        except Exception as e:
+            print("Something went wrong while connecting to the database!", e)
+            exit()
+        
+        # Start the bot
+        await super().start()
+
+        # Handle restart.txt logic
+        if os.path.exists('restart.txt'):
+            with open("restart.txt") as file:
+                chat_id, msg_id = map(int, file)
+            try:
+                await self.edit_message_text(chat_id=chat_id, message_id=msg_id, text='Restarted Successfully!')
+            except:
+                pass
+            os.remove('restart.txt')
+
+        temp.BOT = self
+        await Media.ensure_indexes()
+        me = await self.get_me()
+        temp.ME = me.id
+        temp.U_NAME = me.username
+        temp.B_NAME = me.first_name
+        username = '@' + me.username
+        print(f"{me.first_name} is started now 🤗")
+        
+        # Web server setup
+        app = web.AppRunner(web_app)
+        await app.setup()
+        await web.TCPSite(app, "0.0.0.0", PORT).start()
+
+        # Log the bot start
+        try:
+            await self.send_message(chat_id=LOG_CHANNEL, text=f"<b>{me.mention} Restarted! 🤖</b>")
+        except:
+            print("Error - Make sure bot is admin in LOG_CHANNEL, exiting now")
+            exit()
+
+        # Test message in BIN_CHANNEL
+        try:
+            m = await self.send_message(chat_id=BIN_CHANNEL, text="Test")
+            await m.delete()
+        except:
+            print("Error - Make sure bot is admin in BIN_CHANNEL, exiting now")
+            exit()
+
+        # Notify admins
+        for admin in ADMINS:
+            await self.send_message(chat_id=admin, text=f"<b>✅ ʙᴏᴛ ʀᴇsᴛᴀʀᴛᴇᴅ</b>")
+
+    async def stop(self, *args):
+        await super().stop()
+        print("Bot Stopped! Bye...")
+
+    async def iter_messages(self: Client, chat_id: Union[int, str], limit: int, offset: int = 0) -> Optional[AsyncGenerator["types.Message", None]]:
+        current = offset
+        while True:
+            new_diff = min(200, limit - current)
+            if new_diff <= 0:
+                return
+            messages = await self.get_messages(chat_id, list(range(current, current + new_diff + 1)))
+            for message in messages:
+                yield message
+                current += 1
+
+app = Bot()
+
+try:
+    app.run()
+except FloodWait as e:
+    print(f"FloodWait triggered. Waiting for {e.x} seconds.")
+    asyncio.sleep(e.x)  # Wait for the specified time
+    app.run()  # Retry after the wait time
---
+++ info.py
@@ -0,0 +1,141 @@
+import re, logging
+from os import environ
+from Script import script
+
+def is_enabled(type, value):
+    data = environ.get(type, str(value))
+    if data.lower() in ["true", "yes", "1", "enable", "y"]:
+        return True
+    elif data.lower() in ["false", "no", "0", "disable", "n"]:
+        return False
+    else:
+        print(f'Error - {type} is invalid, exiting now')
+        exit()
+
+def is_valid_ip(ip):
+    ip_pattern = r'\b(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b'
+    return re.match(ip_pattern, ip) is not None
+
+# Bot information
+API_ID = environ.get('API_ID', '27002519')  #api id of your telegram id
+if len(API_ID) == 0:
+    print('Error - API_ID is missing, exiting now')
+    exit()
+else:
+    API_ID = int(API_ID)
+API_HASH = environ.get('API_HASH', '1033ee721101d78366b4ac46aadf3930') #api hash of your telegram id
+if len(API_HASH) == 0:
+    print('Error - API_HASH is missing, exiting now')
+    exit()
+BOT_TOKEN = environ.get('BOT_TOKEN', '') #bot token from botfather
+if len(BOT_TOKEN) == 0:
+    print('Error - BOT_TOKEN is missing, exiting now')
+    exit()
+PORT = int(environ.get('PORT', '80')) #don't change anything 
+
+# Bot pics
+PICS = (environ.get('PICS', 'https://i.postimg.cc/prXPRvsh/IXPs2c-Wx-Tt2-Ep-A5-Dcr6-QQ.webp')).split()
+
+# Bot Admins
+ADMINS = environ.get('ADMINS', '6174868004') #apni tg id daalo
+if len(ADMINS) == 0:
+    print('Error - ADMINS is missing, exiting now')
+    exit()
+else:
+    ADMINS = [int(admins) for admins in ADMINS.split()]
+
+# Channels
+INDEX_CHANNELS = [int(index_channels) if index_channels.startswith("-") else index_channels for index_channels in environ.get('INDEX_CHANNELS', '-1002208295766 -1002233820213 -1002227944470 -1002185234412').split()]
+if len(INDEX_CHANNELS) == 0:
+    print('Info - INDEX_CHANNELS is empty')
+AUTH_CHANNEL = [int(auth_channels) for auth_channels in environ.get('AUTH_CHANNEL', '-1002233212878').split()]
+if len(AUTH_CHANNEL) == 0:
+    print('Info - AUTH_CHANNEL is empty')
+LOG_CHANNEL = environ.get('LOG_CHANNEL', '-1002183671823') #bot log channel -1005293546253
+if len(LOG_CHANNEL) == 0:
+    print('Error - LOG_CHANNEL is missing, exiting now')
+    exit()
+else:
+    LOG_CHANNEL = int(LOG_CHANNEL)
+IS_FSUB = is_enabled('IS_FSUB', True)
+
+# support group
+SUPPORT_GROUP = environ.get('SUPPORT_GROUP', '-1002155777503') #support group id ex:  -1002936246860
+if len(SUPPORT_GROUP) == 0:
+    print('Error - SUPPORT_GROUP is missing, exiting now')
+    exit()
+else:
+    SUPPORT_GROUP = int(SUPPORT_GROUP)
+
+# MongoDB information
+DATABASE_URL = environ.get('DATABASE_URL', "mongodb+srv://ayushpritysingh098:z0aMVL9ofTSOGqir@cluster0.9r9gs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0") #mongo db url
+if len(DATABASE_URL) == 0:
+    print('Error - DATABASE_URL is missing, exiting now')
+    exit()
+DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
+COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Files')
+
+# Links
+SUPPORT_LINK = environ.get('SUPPORT_LINK', 'https://t.me/Netfilix_movie_shaport')
+OWNER_USERNAME = environ.get("OWNER_USERNAME", "https://t.me/Hansaka_Anuhas")
+UPDATES_LINK = environ.get('UPDATES_LINK', 'https://t.me/+Ie4mNf6lL5RjYjM1')
+FILMS_LINK = environ.get('FILMS_LINK', 'https://t.me/n_flixmovie')
+TUTORIAL = environ.get("TUTORIAL", "https://t.me/APSmo/7")
+VERIFY_TUTORIAL = environ.get("VERIFY_TUTORIAL", "")
+
+# Bot settings
+DELETE_TIME = int(environ.get('DELETE_TIME', 300)) # Add time in seconds 
+CACHE_TIME = int(environ.get('CACHE_TIME', 300))
+MAX_BTN = int(environ.get('MAX_BTN', 10)) #don't change anything in Language 
+LANGUAGES = [language.lower() for language in environ.get('LANGUAGES', 'hindi english telugu tamil kannada malayalam marathi punjabi').split()]
+QUALITY = [quality.lower() for quality in environ.get('QUALITY', '360p 480p 720p 1080p 2160p').split()]
+IMDB_TEMPLATE = environ.get("IMDB_TEMPLATE", script.IMDB_TEMPLATE)
+FILE_CAPTION = environ.get("FILE_CAPTION", script.FILE_CAPTION)
+SHORTLINK_URL = environ.get("SHORTLINK_URL", "shortxlinks.com")
+SHORTLINK_API = environ.get("SHORTLINK_API", "fa2a0768fc8d2a51b22e46293634a52670a73c7a")
+VERIFY_EXPIRE = int(environ.get('VERIFY_EXPIRE', 86400)) # Add time in seconds
+WELCOME_TEXT = environ.get("WELCOME_TEXT", script.WELCOME_TEXT)
+INDEX_EXTENSIONS = [extensions.lower() for extensions in environ.get('INDEX_EXTENSIONS', 'mp4 mkv').split()]
+STICKERS_IDS = (
+    "CAACAgQAAxkBAAEK99dlfC7LDqnuwtGRkIoacot_dGC4zQACbg8AAuHqsVDaMQeY6CcRojME CAACAgQAAxkBAAELqxll8CcG-MZx9mIOXgaHSzLc9uyxswACaxQAAlrdEVOJDG3cIZuWLzQE"
+).split()
+
+# boolean settings 
+GROUP_FSUB = is_enabled('GROUP_FSUB', False) 
+PM_SEARCH = is_enabled('PM_SEARCH', False) #switch True or False for searching results in bot pm😃
+IS_VERIFY = is_enabled('IS_VERIFY', False)
+AUTO_DELETE = is_enabled('AUTO_DELETE', True)
+WELCOME = is_enabled('WELCOME', False)
+PROTECT_CONTENT = is_enabled('PROTECT_CONTENT', False)
+LONG_IMDB_DESCRIPTION = is_enabled("LONG_IMDB_DESCRIPTION", False)
+LINK_MODE = is_enabled("LINK_MODE", True)
+AUTO_FILTER = is_enabled('AUTO_FILTER', True)
+IMDB = is_enabled('IMDB', False)
+SPELL_CHECK = is_enabled("SPELL_CHECK", True)
+SHORTLINK = is_enabled('SHORTLINK', True)
+
+#premium info
+PAYMENT_QR = environ.get('PAYMENT_QR', 'https://i.postimg.cc/NFbZ4r7L/Google-Pay-QR-2.png')
+OWNER_UPI_ID = environ.get('OWNER_UPI_ID', 'singhayush88805@okaxis')
+
+# for stream
+IS_STREAM = is_enabled('IS_STREAM', True) #true if you want stream feature active in your bot
+BIN_CHANNEL = environ.get("BIN_CHANNEL", "-1002237538640") #if is_stream = true then add a channel id ex: -10026393639
+if len(BIN_CHANNEL) == 0:
+    print('Error - BIN_CHANNEL is missing, exiting now')
+    exit()
+else:
+    BIN_CHANNEL = int(BIN_CHANNEL)
+URL = environ.get("URL", "https://t.me/+kP4LSVcocBVhZjc1") #if heroku then paste the app link here ex: https://heroku......./
+if len(URL) == 0:
+    print('Error - URL is missing, exiting now')
+    exit()
+else:
+    if URL.startswith(('https://', 'http://')):
+        if not URL.endswith("/"):
+            URL += '/'
+    elif is_valid_ip(URL):
+        URL = f'http://{URL}/'
+    else:
+        print('Error - URL is not valid, exiting now')
+        exit()
