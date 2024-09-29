83b0111337a313d463bec8a74237a2a20c3fa16c
ary43434 <aryankr101082@gmail.com>
2024-09-29 12:59:37 +0530

    Update info.py

--- info.py
+++ info.py
@@ -102,8 +102,8 @@ STICKERS_IDS = (
 ).split()
 
 # boolean settings 
-GROUP_FSUB = is_enabled('GROUP_FSUB', False) 
-PM_SEARCH = is_enabled('PM_SEARCH', False) #switch True or False for searching results in bot pm😃
+GROUP_FSUB = is_enabled('GROUP_FSUB', True) 
+PM_SEARCH = is_enabled('PM_SEARCH', True) #switch True or False for searching results in bot pm😃
 IS_VERIFY = is_enabled('IS_VERIFY', False)
 AUTO_DELETE = is_enabled('AUTO_DELETE', True)
 WELCOME = is_enabled('WELCOME', False)
